4052bfe207d78b9ebb936ec898377f8ba19cb18a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-08-30 20:56:30 -0300

    refactor(clean): remove unecessary code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400'],
+  display: 'swap',
+  variable: '--font-roboto',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='pt-br'>
+      <body className={roboto.variable}>{children}</body>
     </html>
   )
 }
