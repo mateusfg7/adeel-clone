4733b4cab5894f1ff42a53e86a09cbf325ff1f18
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-08-30 23:00:58 -0300

    feat(ui): add smoth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='pt-br'>
+    <html lang='pt-br' className='scroll-smooth'>
       <body className={`${roboto.variable} ${notoSerifTelugu.variable}`}>
         {children}
       </body>
